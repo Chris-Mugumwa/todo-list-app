922b8b47696df6390c59262ded4f1d2c87235a02
Ngonidzashe Christian Mugumwa <ngoni.mugumwa@gmail.com>
2020-09-24 17:44:52 +0200

    Need to fix a few bugs

--- app.js
+++ app.js
@@ -8,16 +8,28 @@ const list = document.querySelector('.main__list');
 
 button.addEventListener('click', todoAdd);
 
-//functions 
+//functions
 
 function todoAdd(e) {
-    //preventing the form form submitting
-    event.preventDefault();    
-    //Todo Div
-    const mainTodo = document.createElement('div');
-    mainTodo.classList.add('todo');
-    //create Li
-    const mainList = document.createElement('li');
-    mainList = 'hey';
-    mainList.classList.add('main__items');
+	//preventing the form form submitting
+	event.preventDefault();
+	//todo Div
+	const todoDiv = document.createElement('div');
+	todoDiv.classList.add('todo');
+	//create Li
+	const newTodo = document.createElement('li');
+	newTodo.innerText = this.value;
+	newTodo.classList.add('main__item');
+    todoDiv.appendChild(newTodo);
+    //completed button
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML = '<ion-icon name="checkmark"></ion-icon>';
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+    //delete button
+    const deleteButton = document.querySelector('button');
+    deleteButton.innerHTML = '<ion-icon name="trash-bin-outline"></ion-icon>';
+    todoDiv.appendChild(deleteButton);
+    //append
+    list.appendChild(todoDiv);
 }
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,22 +1,32 @@
 "use strict";
 
-function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
-
 //Selectors
 var input = document.querySelector('.main__input');
 var button = document.querySelector('.main__button');
 var list = document.querySelector('.main__list'); //event listeners
 
-button.addEventListener('click', todoAdd); //functions 
+button.addEventListener('click', todoAdd); //functions
 
 function todoAdd(e) {
   //preventing the form form submitting
-  event.preventDefault(); //Todo Div
+  event.preventDefault(); //todo Div
 
-  var mainTodo = document.createElement('div');
-  mainTodo.classList.add('todo'); //create Li
+  var todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo'); //create Li
 
-  var mainList = document.createElement('li');
-  mainList = (_readOnlyError("mainList"), 'hey');
-  mainList.classList.add('main__items');
+  var newTodo = document.createElement('li');
+  newTodo.innerText = this.value;
+  newTodo.classList.add('main__item');
+  todoDiv.appendChild(newTodo); //completed button
+
+  var completeButton = document.createElement('button');
+  completeButton.innerHTML = '<ion-icon name="checkmark"></ion-icon>';
+  completeButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton); //delete button
+
+  var deleteButton = document.querySelector('button');
+  deleteButton.innerHTML = '<ion-icon name="trash-bin-outline"></ion-icon>';
+  todoDiv.appendChild(deleteButton); //append
+
+  list.appendChild(todoDiv);
 }
